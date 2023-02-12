b627f8573c29fedf7e55a8e8bb473f5bb349ac64
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-02-13 01:04:31 +0900

    Run go fmt

--- tokenizer_test.go
+++ tokenizer_test.go
@@ -8,37 +8,37 @@ import (
 
 func TestNew(t *testing.T) {
 	tests := []struct {
-		name string
-		inputS string
-		inputU bool 
+		name      string
+		inputS    string
+		inputU    bool
 		inputLoop int
-		outputCPs []uint 
+		outputCPs []uint
 	}{
 		{
-			name: "ユニコードモードで、2回 Next を呼び出したときに `あ`, `い`, `う` のコードポイントを CP で参照できる",
-			inputS: "あいう",
-			inputU: true,
+			name:      "ユニコードモードで、2回 Next を呼び出したときに `あ`, `い`, `う` のコードポイントを CP で参照できる",
+			inputS:    "あいう",
+			inputU:    true,
 			inputLoop: 2,
 			outputCPs: []uint{0x3042, 0x3044, 0x3046},
 		},
 		{
-			name: "非ユニコードモードで、2回 Next を呼び出したときに `あ`, `い`, `う` のコードポイントを CP で参照できる",
-			inputS: "あいう",
-			inputU: false,
+			name:      "非ユニコードモードで、2回 Next を呼び出したときに `あ`, `い`, `う` のコードポイントを CP で参照できる",
+			inputS:    "あいう",
+			inputU:    false,
 			inputLoop: 2,
 			outputCPs: []uint{0x3042, 0x3044, 0x3046},
 		},
 		{
-			name: "ユニコードモードで、2回 Next を呼び出したときに `あ`, `い`, `𠮟` のコードポイントを CP で参照できる",
-			inputS: "あい𠮟",
-			inputU: true,
+			name:      "ユニコードモードで、2回 Next を呼び出したときに `あ`, `い`, `𠮟` のコードポイントを CP で参照できる",
+			inputS:    "あい𠮟",
+			inputU:    true,
 			inputLoop: 2,
 			outputCPs: []uint{0x3042, 0x3044, 0x20b9f},
 		},
 		{
-			name: "非ユニコードモードで、3回 Next を呼び出したときに `あ`, `い`, `𠮟`の前半部, `𠮟`の後半部 のコードポイントを CP で参照できる",
-			inputS: "あい𠮟",
-			inputU: false,
+			name:      "非ユニコードモードで、3回 Next を呼び出したときに `あ`, `い`, `𠮟`の前半部, `𠮟`の後半部 のコードポイントを CP で参照できる",
+			inputS:    "あい𠮟",
+			inputU:    false,
 			inputLoop: 3,
 			outputCPs: []uint{0x3042, 0x3044, 0xd842, 0xdf9f},
 		},
