1deeb4be702edd5bbbfe9c658b614bab145a3c9c
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-02-13 00:34:52 +0900

    Add `width`

--- tokenizer.go
+++ tokenizer.go
@@ -4,10 +4,16 @@ type Tokenizer struct {
 	// 文字コードに関するユーティリティ
 	// ユニコードモードかどうかによって実装が異なる
 	cu *CharCodeUtils
+
 	// 現在見ている位置
 	i int
+
+	// 現在見ている文字の width
+	w int
+
 	// ソースの文字列
 	s string
+
 	// 現在のコードポイント
 	// ユニコードモードでない場合はコードユニットの場合もある
 	CP uint
@@ -20,11 +26,17 @@ func NewTokenizer(s string, u bool) Tokenizer {
 	} else {
 		cu = &LegacyCharUtils{}
 	}
+
 	i := 0
+	cp := cu.At(s, i)
+	w := cu.Width(cp)
 	return Tokenizer{
 		cu: &cu,
 		i:  i,
+		w:  w,
 		s:  s,
-		CP: cu.At(s, i),
+		CP: cp,
 	}
 }
+
+func (t *Tokenizer) Next() {}
