5e1b6dd398d8ea3eb2fdb0653b876693b3d79c98
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-02-15 02:31:05 +0900

    Add `Disjunction` node

--- ast.go
+++ ast.go
@@ -1,1 +1,21 @@
 package regexpp
+
+type Node struct {
+	Data N
+	Loc  Loc
+}
+
+type Loc struct {
+	start int
+	end   int
+}
+
+type N interface {
+	isNode()
+}
+
+func (n *NDisjunction) isNode() {}
+
+type NDisjunction struct {
+	elements []Node
+}
