0ff4b13f77e9de2ebeddef520c3248a1aec30aa7
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-02-17 00:18:53 +0900

    Add `Pattern` node

--- ast.go
+++ ast.go
@@ -14,9 +14,14 @@ type N interface {
 	isNode()
 }
 
+func (n *NPattern) isNode()     {}
 func (n *NDisjunction) isNode() {}
 func (n *NAlternative) isNode() {}
 func (n *NAtom) isNode()        {}
+
+type NPattern struct {
+	Value *Node
+}
 
 type NDisjunction struct {
 	Left  *Node
@@ -29,5 +34,5 @@ type NAlternative struct {
 }
 
 type NAtom struct {
-	value *Node
+	Value *Node
 }
