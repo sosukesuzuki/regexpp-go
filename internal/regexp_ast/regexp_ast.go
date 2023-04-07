0e227014a918663d8201fc8ac3be6e2f436f458d
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-04-07 22:55:32 +0900

    Fix

--- internal/regexp_ast/regexp_ast.go
+++ internal/regexp_ast/regexp_ast.go
@@ -1,22 +1,17 @@
 package regexp_ast
 
-type Node struct {
-	Data N
-	Loc  Loc
-}
-
 type Loc struct {
 	Start int
 	End   int
 }
 
-type N interface {
-	isN()
+type Node interface {
+	isNode()
 }
 
-func (n *Pattern) isN()     {}
-func (n *Alternative) isN() {}
-func (n *Character) isN()   {}
+func (n *Pattern) isNode()     {}
+func (n *Alternative) isNode() {}
+func (n *Character) isNode()   {}
 
 type Element interface {
 	isElement()
@@ -26,12 +21,15 @@ func (n *Character) isElement() {}
 
 type Pattern struct {
 	Alternatives []Alternative
+	Loc Loc
 }
 
 type Alternative struct {
 	Elements []Element
+	Loc Loc
 }
 
 type Character struct {
 	value uint
+	Loc Loc
 }
