79a34f91d1769c4ead1071ddfee988088be3735e
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-04-07 23:37:22 +0900

    Parser Disjunction

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -3,6 +3,7 @@ package parser
 import (
 	"github.com/sosukesuzuki/regexpp-go/internal/lexer"
 	"github.com/sosukesuzuki/regexpp-go/internal/regexp_ast"
+	"github.com/sosukesuzuki/regexpp-go/internal/unicode_consts"
 )
 
 type Parser struct {
@@ -26,9 +27,12 @@ func (p *Parser) ParsePattern() *regexp_ast.Pattern {
 	return p.pattern
 }
 
+// Pattern
+
 func (p *Parser) consumePattern() {
 	start := p.lexer.I
 	p.onPatternEnter(start)
+	p.consumeDisjunction()
 	p.onPatternLeave(start, p.lexer.I)
 }
 
@@ -45,3 +49,37 @@ func (p *Parser) onPatternEnter(start int) {
 func (p *Parser) onPatternLeave(start int, end int) {
 	p.node.SetEnd(end)
 }
+
+// Disjunction
+
+func (p *Parser) consumeDisjunction() {
+	start := p.lexer.I
+	p.onDisjunctionEnter(start)
+
+	i := 0
+	for {
+		p.consumeAlternative(i)
+		i = i + 1
+		if !p.lexer.Eat(unicode_consts.VerticalLine) {
+			break
+		}
+	}
+
+	p.onDisjunctionLeave(start, p.lexer.I)
+}
+
+func (p *Parser) onDisjunctionEnter(start int) {
+	// do nothing
+}
+
+func (p *Parser) onDisjunctionLeave(start int, end int) {
+	// do nothing
+}
+
+// Alternative
+
+func (p *Parser) consumeAlternative(index int) {}
+
+func (p *Parser) onAlternativeEnter(start int) {}
+
+func (p *Parser) onAlternativeLeave(start int, end int) {}
